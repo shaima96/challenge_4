fbf96fa6ce78dab71bee6879d85ae0a68d665571
shaima96 <141062@ppu.edu.ps>
2020-11-05 12:33:18 +0200

    By works,I mean't doesnt

--- server.js
+++ server.js
@@ -1,2 +1,24 @@
 
 
+const express = require('express');
+const app = express();
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static(__dirname + '/./public'))
+app.use(express.json())
+
+
+
+
+
+app.get("/app", (req, res) => {
+    res.sendFile(__dirname + '/./client/dist/bundle.js')
+})
+
+
+
+let port = 4001;
+
+app.listen(port, () => {
+    console.log(`listening to port 4001`);
+});
